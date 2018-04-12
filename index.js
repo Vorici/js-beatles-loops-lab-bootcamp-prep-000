4b46236b48273f6455afe7e400e224e877d9be8e
Ledio Bode <l.bode515@gmail.com>
2018-04-12 08:29:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-var musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
 
-var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
 
-var facts = [
+const facts = [
   "He was the last Beatle to learn to drive",
   "He was never a vegetarian",
   "He was a choir boy and boy scout",
   "He hated the sound of his own voice"
 ];
 
+const number = 0;
 
 function theBeatlesPlay(musicians, instruments) {
   
@@ -42,5 +43,6 @@ function iLoveBeatles(number){
   
   var array = [];
   
+  var i = number;
   
 }
