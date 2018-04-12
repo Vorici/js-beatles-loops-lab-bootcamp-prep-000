8bb7e20cc9d0b0744a7529a288c99ee06104d795
Ledio Bode <l.bode515@gmail.com>
2018-04-12 08:24:58 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,39 @@
+var musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+
+var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+
+var facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+
+function theBeatlesPlay(musicians, instruments) {
+  
+  var array = [];
+  for (let i = 0; i < musicians.length; i++) {
+     array.push(musicians[i] + " plays " + instruments[i]);
+     
+    }
+ 
+ return array;
+ 
+}
+
+
+function johnLennonFacts(facts) {
+  
+  var exclamationFacts = [];
+  
+  var i = 0;
+  
+  while (i < facts.length) {
+    
+    exclamationFacts.push(facts[i] + "!!!" );
+    
+    i++ ; }  
+  
+  return exclamationFacts;
+  
+}
